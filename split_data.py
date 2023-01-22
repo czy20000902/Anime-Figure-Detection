d104f0d3d6655bafff0b76babad7c80391a9c4c3
czy20000902 <60259729+czy20000902@users.noreply.github.com>
2023-01-22 21:55:44 +0100

    Add files via upload

---
+++ split_data.py
@@ -0,0 +1,33 @@
+import os
+import shutil
+
+
+output_path = 'voc_dataset_output/'
+data_path = 'voc_dataset_original/'
+paths = ['train', 'test', 'valid']
+
+try:
+    os.makedirs(output_path + 'Annotations')
+except:
+    print('Dir Annotations already exists')
+try:
+    os.makedirs(output_path + 'ImageSets/Main')
+except:
+    print('Dir ImageSets/Main already exists')
+try:
+    os.makedirs(output_path + 'JPEGImages')
+except:
+    print('Dir JPEGImages already exists')
+
+for path in paths:
+
+    xmlfilepath = os.path.join(data_path, path) + '/Annotations/'
+    imagepath = os.path.join(data_path, path) + '/JPEGImages/'
+    txtsavepath = output_path + '/ImageSets/Main/'
+    total_xml = os.listdir(xmlfilepath)
+
+
+    file = open(txtsavepath + path + '.txt', 'w')
+    for line in total_xml:
+        file.write(line[:-4]+'\n')
+        shutil.copy(imagepath + line[:-4] + '.jpg', output_path + '/JPEGImages/')
